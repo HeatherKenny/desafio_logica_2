3efa72ce0153e3301d67962feb8a04f7b514eaf2
HeatherKenny <beatriz.r.freotas7@gmail.com>
2024-10-09 12:22:05 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function calcularNivel(vitorias, derrotas) {
+    // Calcula o saldo de vitórias
+    const saldoVitorias = vitorias - derrotas;
+  
+    // Determina o nível com base no saldo   
+  
+    let nivel;
+    if (saldoVitorias < 10) {
+      nivel = "Ferro";
+    } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+      nivel = "Bronze";
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+      nivel = "Prata";
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+      nivel = "Ouro";
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+      nivel = "Diamante";
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+      nivel = "Lendário";
+    } else {
+      nivel
+   = "Imortal";
+    }
+  
+    // Exibe a mensagem   
+   final
+    console.log(`O herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
+  }
+  
+  // Exemplo de uso:
+  calcularNivel(30, 10); // Saída: O herói tem de saldo de 20 está no nível de Prata
